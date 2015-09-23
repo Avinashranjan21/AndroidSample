f0532be985c58c38529f01fde37e7c097c4476af
Avinash Ranjan <Avinash.ranjan21@gmail.com>
2015-09-23 23:38:33 +0530

    Navigation drawer hello world

--- LoadImageFromUrl/app/src/main/java/com/approx/asynctask1/MainActivity.java
+++ LoadImageFromUrl/app/src/main/java/com/approx/asynctask1/MainActivity.java
@@ -1,14 +1,38 @@
 package com.approx.asynctask1;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView= (ImageView) findViewById(R.id.imageView);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
+    private class ImageDownloader extends AsyncTask<String ,String ,Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+
+            return null;
+        }
     }
 
 }
