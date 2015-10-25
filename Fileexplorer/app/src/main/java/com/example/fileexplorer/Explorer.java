cd9fe8a6b573deb76d79d5dc3c26b736c4ddb488
Avinash Ranjan <Avinash.ranjan21@gmail.com>
2015-10-26 00:25:26 +0530

    File Explorer added

---
+++ Fileexplorer/app/src/main/java/com/example/fileexplorer/Explorer.java
@@ -0,0 +1,112 @@
+package com.example.fileexplorer;
+
+import java.io.File;
+import java.sql.Date;
+import java.util.ArrayList; 
+import java.util.Collections;
+import java.util.List;
+import java.text.DateFormat;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class Explorer extends ListActivity {
+
+	private File currentDir;
+    private ExplorerArrayAdapter adapter;
+	Bundle extras;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); 
+        currentDir = new File(Environment.getExternalStorageDirectory().getPath());
+        fill(currentDir);
+		extras = getIntent().getExtras();
+    }
+    private void fill(File f)
+    {
+    	File[]dirs = f.listFiles(); 
+		 this.setTitle("Current Dir: "+f.getName());
+		 List<Item>dir = new ArrayList<Item>();
+		 List<Item>fls = new ArrayList<Item>();
+		 try{
+			 for(File ff: dirs)
+			 { 
+				Date lastModDate = new Date(ff.lastModified()); 
+				DateFormat formater = DateFormat.getDateTimeInstance();
+				String date_modify = formater.format(lastModDate);
+				if(ff.isDirectory()){
+					
+					
+					File[] fbuf = ff.listFiles(); 
+					int buf = 0;
+					if(fbuf != null){ 
+						buf = fbuf.length;
+					} 
+					else buf = 0; 
+					String num_item = String.valueOf(buf);
+					if(buf == 0) num_item = num_item + " item";
+					else num_item = num_item + " items";
+					
+					//String formated = lastModDate.toString();
+					dir.add(new Item(ff.getName(),num_item,date_modify,ff.getAbsolutePath(),"directory_icon")); 
+				}
+				else
+				{
+					
+					fls.add(new Item(ff.getName(),ff.length() + " Byte", date_modify, ff.getAbsolutePath(),"file_icon"));
+				}
+			 }
+		 }catch(Exception e)
+		 {    
+			 
+		 }
+		 Collections.sort(dir);
+		 Collections.sort(fls);
+		 dir.addAll(fls);
+		 if(!f.getName().equalsIgnoreCase("sdcard"))
+			 dir.add(0,new Item("..","Parent Directory","",f.getParent(),"directory_up"));
+		 adapter = new ExplorerArrayAdapter(Explorer.this,R.layout.file_view,dir);
+		 this.setListAdapter(adapter); 
+    }
+    @Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+
+		 if (extras.getInt("REQUEST_CODE")==1){
+			 Item o = adapter.getItem(position);
+			 if(o.getImage().equalsIgnoreCase("directory_icon")||o.getImage().equalsIgnoreCase("directory_up")){
+				 currentDir = new File(o.getPath());
+
+				 fill(currentDir);
+			 }
+			 else
+			 {
+				 onFileClick(o);
+			 }
+		 } else if(extras.getInt("REQUEST_CODE")==2){
+			 Item o = adapter.getItem(position);
+			 File file = new File(o.getPath());
+			 if (file.isDirectory()){
+				 onFileClick(o);
+			 }else {
+				 Toast.makeText(getApplicationContext(),"Please Select Folder",Toast.LENGTH_SHORT).show();
+			 }
+		 }
+
+	}
+    private void onFileClick(Item o)
+    {
+    	Intent intent = new Intent();
+        intent.putExtra("GetPath",currentDir.toString());
+        intent.putExtra("GetFilePath",o.getPath().toString());
+        intent.putExtra("GetFileName",o.getName());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
---
+++ Fileexplorer/app/src/main/java/com/example/fileexplorer/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.fileexplorer;
+  
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity; 
+import android.content.Intent;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+ 
+	private static final int REQUEST_SOURCE = 1;
+	private static final int REQUEST_DESTINATION = 2;
+
+	String curFileName,file_name;
+	Button select_source,Select_destination,select_compress;
+	EditText edittext_source,edittext_destination;
+	ImageView select_image;
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fileexplorer);
+		edittext_source = (EditText)findViewById(R.id.editText);
+		edittext_destination = (EditText)findViewById(R.id.editText_destination);
+		select_image= (ImageView) findViewById(R.id.select_image);
+		select_source= (Button) findViewById(R.id.select_source);
+		Select_destination= (Button) findViewById(R.id.select_destination);
+		select_compress= (Button) findViewById(R.id.select_compress);
+		select_source.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				getSource(view);
+			}
+		});
+		Select_destination.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				getDestination(view);
+			}
+		});
+		select_compress.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+			}
+		});
+    }
+ 
+    public void getSource(View view){
+    	Intent intent1 = new Intent(this, Explorer.class);
+		intent1.putExtra("REQUEST_CODE", REQUEST_SOURCE);
+        startActivityForResult(intent1, REQUEST_SOURCE);
+    }
+	public void getDestination(View view){
+    	Intent intent1 = new Intent(this, Explorer.class);
+		intent1.putExtra("REQUEST_CODE",REQUEST_DESTINATION);
+        startActivityForResult(intent1,REQUEST_DESTINATION);
+    }
+ // Listen for results.
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        // See which child activity is calling us back.
+    	if (requestCode == REQUEST_SOURCE){
+    		if (resultCode == RESULT_OK) {
+    			curFileName = data.getStringExtra("GetFilePath");
+				file_name = data.getStringExtra("GetFileName");
+				edittext_source.setText(curFileName);
+				Uri myUri = Uri.parse(curFileName);
+    		}
+    	 } else if (requestCode == REQUEST_DESTINATION){
+			if (resultCode == RESULT_OK) {
+				curFileName = data.getStringExtra("GetFilePath");
+				file_name = data.getStringExtra("GetFileName");
+				edittext_destination.setText(curFileName);
+			}
+		}
+    }
+	private void compress_bitmap(Uri bitmap_uri){
+		try {
+			Bitmap mBitmap =  MediaStore.Images.Media.getBitmap(this.getContentResolver(), bitmap_uri);
+			getResizedBitmap(mBitmap,50,50);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+		int width = bm.getWidth();
+		int height = bm.getHeight();
+		float scaleWidth = ((float) newWidth) / width;
+		float scaleHeight = ((float) newHeight) / height;
+		Matrix matrix = new Matrix();
+		matrix.postScale(scaleWidth, scaleHeight);
+		Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height,matrix, false);
+
+		return resizedBitmap;
+	}
+
+	private void createDirectoryAndSaveFile(Bitmap imageToSave, String fileName,Uri save_destination) {
+
+		File direct = new File(save_destination.getPath());
+
+		File file = new File(new File(save_destination.getPath()), fileName);
+		if (file.exists()) {
+			file.delete();
+		}
+		try {
+			FileOutputStream out = new FileOutputStream(file);
+			imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+}
